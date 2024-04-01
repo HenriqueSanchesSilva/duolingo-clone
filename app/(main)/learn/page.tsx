452c6c5d51931c0da45e441f940b44e4e3d4a607
Henrique Silva <henriquessilva463@gmail.com>
2024-03-31 23:42:20 -0300

    feat header & progressbar

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,17 +1,25 @@
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { FeedWrapper } from "@/components/feed-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
-                My Stick Wrapper
+                <UserProgress 
+                    activeCourse={{title: "Spanish", imageSrc: "/es.svg"}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
             </StickyWrapper>
             <FeedWrapper>
-                <Header />
+                <Header title="Spanish"/>
+
             </FeedWrapper>
         </div>
     );
 };
 
-export default LearnPage;
+export default LearnPage; 
